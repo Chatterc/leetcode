761af76773eede7435c2ccbf2e3278765ad52c48
Carl Chatterton <Carl.Chatterton@maellin.io>
2022-01-07 10:26:23 -0500

    added solution for preorder traversal of BT

---
+++ problems/Binary-tree-preorder-traversal.py
@@ -0,0 +1,70 @@
+# Problem - Given the root of a binary tree, return the inorder traversal of its nodes' values.
+#
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+# 
+# Example 2:
+# Input: root = []
+# Output: []
+#
+# Example 3:
+# Input: root = [1]
+# Output: [1]
+#
+# Constraints:
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    # Insert method for new Node
+    def insert(self, val):
+        if self.val:
+            # Creates a new node if the val is None
+            if not val:
+                return TreeNode(val)
+
+            if val < self.val:
+                if self.left is None:
+                    self.left = TreeNode(val)
+                else:
+                    self.left.insert(val)
+                    
+            elif val > self.val:
+                if self.right is None:
+                    self.right = TreeNode(val)
+                else:
+                    self.right.insert(val)
+                    
+            else:
+                self.val = val
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if root:
+            res.append(root.val)
+            res = res + self.preorderTraversal(root.left)
+            res = res + self.preorderTraversal(root.right)
+        return res
+    
+def main():
+    root = TreeNode(1)
+    root.insert(None)
+    root.insert(2)
+    root.insert(3)
+    
+    ans = Solution().preorderTraversal(root=root)
+    print(ans) # Expected result [1, 3, 2]
+
+if __name__ == '__main__':
+    main()
+    
